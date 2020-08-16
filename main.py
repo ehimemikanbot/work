688d9beba0f8d95f99de33c40ce2c8b66595356f
ehimemikanbot <sunrise9love2is3all2blue@gmail.com>
2020-08-16 14:35:03 +0900

    '返信作成'

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def handle_message(event):
     #    TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
     line_bot_api.reply_message(
         event.reply_token,
-        FlexSendMessage(alt_text="items",contents=BubbleContainer.new_from_json_dict(json.load(menu)))
+        FlexSendMessage(alt_text="items",contents=container_obj)
     )
  
 # ポート番号の設定
