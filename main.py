6acf3c42d974925bc546891dff9cb8aa76e9aa29
ehimemikanbot <sunrise9love2is3all2blue@gmail.com>
2020-08-29 19:16:22 +0900

    otameshi5

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ def handle_message(event):
     #    TextSendMessage(text=event.message.text)) #ここでオウム返しのメッセージを返します。
     line_bot_api.reply_message(
         event.reply_token,
-        FlexSendMessage(alt_text='hoge',container_obj)
+        FlexSendMessage(alt_text='hoge',contents=container_obj)
     )
  
 # ポート番号の設定
